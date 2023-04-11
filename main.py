64e65076e107ac7d3643da9aa61cd6d05782033a
Chapi777 <pomido77@yandex.ru>
2023-04-11 11:54:52 +0300

    Fixed kod

--- main.py
+++ main.py
@@ -1,110 +1,166 @@
-from tkinter import *
-from tkinter import ttk
-import tkinter.messagebox as mb
 import datetime
-import pymysql
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import *
+
+import pymysql
+
 from connect import host, user, password, db_name
 
 connection = pymysql.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=db_name,
-        cursorclass=pymysql.cursors.DictCursor
-    )
+    host=host,
+    user=user,
+    password=password,
+    database=db_name,
+    cursorclass=pymysql.cursors.DictCursor
+)
+
 
 def zapolnenie():
-        window2 = Tk()
-        window2.title("Форма заполнения")
-        window2.geometry('300x200+700+300')
-        window2.resizable(False, False)
-        window2.configure(background='#BF7F30')
-        txt_1 = Entry(window2, width=20, bg='#BF9930')
-        txt_1.grid(row=5, column=2)
+    global result
+    result = []
 
+    window2 = Tk()
+    window2.title("Форма заполнения")
+    window2.geometry('300x200+700+300')
+    window2.resizable(False, False)
+    window2.configure(background='#BF7F30')
+
+    txt_1 = Entry(window2, width=20, bg='#BF9930')
+    txt_1.grid(row=5, column=2)
+
+    varibale1 = tk.StringVar()
+    varibale1.set("Выберите должность")
+
+    def get_doljnost():
         with connection.cursor() as cursor:
-            prof = f"SELECT id_profs, name_profs FROM profesion"
-            cursor.execute(prof)
-            profs = cursor.fetchall()
-            for i in profs:
-                doljnosti = [i['id_dolj']]
+            sql = f"SELECT id_dolj, name_dolj FROM doljnost"
+            cursor.execute(sql)
+            res = cursor.fetchall()
+            arg = {}
+            for i in res:
+                arg[i["name_dolj"]] = [i["id_dolj"]]
+            return arg
 
-        combobox = ttk.Combobox(values=doljnosti)
-        combobox.place()
-        txt_3 = Entry(window2, width=20, bg='#BF9930')
-        txt_3.grid(row=8, column=2)
-        txt_5 = Entry(window2, width=20, bg='#BF9930')
-        txt_5.grid(row=9, column=2)
-        txt_6 = Entry(window2, width=20, bg='#BF9930')
-        txt_6.grid(row=10, column=2)
-        lbl_one1 = Label(window2, text='ФИО',bg='#BF7F30').grid(row=5, column=1)
-        lbl_one2 = Label(window2, text='Должность',bg='#BF7F30').grid(row=7, column=1)
-        lbl_one3 = Label(window2, text='Актуальность',bg='#BF7F30').grid(row=8, column=1)
-        lbl_one4 = Label(window2, text='Профессия',bg='#BF7F30').grid(row=9, column=1)
-        lbl_one4 = Label(window2, text='Образование',bg='#BF7F30').grid(row=10, column=1)
-        lbl_one5 = Label(window2, text='Уровень владения ПК', bg='#BF7F30').grid(row=10, column=1)
+    arg1 = get_doljnost()
+    val = []
+    for i in arg1:
+        val.append(i)
 
+    def add_doljnost(var):
+        result.append(varibale1.get())
+        print(result)
 
-        def zapolnenie2():
-            with connection.cursor() as cursor:
-                fior = txt_1.get()
-                doljr = txt_2.get()
-                actualr = txt_3.get()
-                profr = txt_4.get()
-                obrazr = txt_5.get()
-                levelr = txt_6.get()
+    combobox1 = tk.OptionMenu(window2, varibale1, *val, command=add_doljnost)
+    combobox1.grid(row=7, column=2)
 
-                now = datetime.datetime.now()
-                k = (now.strftime("%Y-%m-%d %H:%M"))
+    txt_3 = Entry(window2, width=20, bg='#BF9930')
+    txt_3.grid(row=8, column=2)
 
-                if fior == '' or loginr == '' or passwordr == '' or numberphoner == '' or emailr == '' or passportr == '':
-                    msg = "Поля не заполнены"
-                    mb.showerror("Ошибка", msg)
-                else:
-                    sqll = f"SELECT * FROM `users`"
-                    cursor.execute(sqll)
-                    ress = cursor.fetchall()
-                    for i in ress:
-                        loginsr = i['login']
-                        print(loginsr)
-                        if loginsr == loginr:
-                            msg = "Данный логин уже существует!"
-                            mb.showerror("Ошибка", msg)
-                    if len(password) < 8:
-                        msg = "Невозможный пароль!"
-                        mb.showerror("Ошибка", msg)
-                    if len(password) >= 8:
-                            vak = f"INSERT INTO `users` (FIO,login,password,email,phone,passport) VALUES ('{fior}','{loginr}','{passwordr}','{numberphoner}','{emailr}','{passportr}')"
-                            cursor.execute(vak)
-                            connection.commit()
-                            msg = "Вакансия отправлена!"
+    varibale2 = tk.StringVar()
+    varibale2.set("Выберите профессию")
 
-                            mb.showinfo("Информация", msg)
-                            window2.destroy()
+    def get_profession():
+        with connection.cursor() as cursor:
+            sql = f"SELECT id_profs, name_profs FROM profesion"
+            cursor.execute(sql)
+            res = cursor.fetchall()
+            arg = {}
+            for i in res:
+                arg[i["name_profs"]] = [i["id_profs"]]
+            return arg
 
+    arg2 = get_profession()
+    val = []
+    for i in arg2:
+        val.append(i)
 
-        btn_reg = Button(window2, text='Отправить', command=zapolnenie2,bg='#BF9930').grid(row=12, column=2)
-        window2.mainloop()
+    def add_profession(var):
+        result.append(varibale2.get())
+        print(result)
+
+    combobox2 = tk.OptionMenu(window2, varibale2, *val, command=add_profession)
+    combobox2.grid(row=9, column=2)
+
+    varibale3 = tk.StringVar()
+    varibale3.set("Выберите образование")
+
+    def get_obrazovanie():
+        with connection.cursor() as cursor:
+            sql = f"SELECT id_obraz, name_obraz FROM obrazovanie"
+            cursor.execute(sql)
+            res = cursor.fetchall()
+            arg = {}
+            for i in res:
+                arg[i["name_obraz"]] = [i["id_obraz"]]
+            return arg
+
+    arg3 = get_obrazovanie()
+    val = []
+    for i in arg3:
+        val.append(i)
+
+    def add_obrazovanie(var):
+        result.append(varibale3.get())
+        print(result)
+
+    combobox3 = tk.OptionMenu(window2, varibale3, *val, command=add_obrazovanie)
+    combobox3.grid(row=10, column=2)
+
+    varibale4 = tk.StringVar()
+    varibale4.set("Уровень владения ПК")
+
+    def get_levc():
+        with connection.cursor() as cursor:
+            sql = f"SELECT id_levc, name_levc FROM level_vlad_comp"
+            cursor.execute(sql)
+            res = cursor.fetchall()
+            arg = {}
+            for i in res:
+                arg[i["name_levc"]] = [i["id_levc"]]
+            return arg
+
+    arg4 = get_levc()
+    val = []
+    for i in arg4:
+        val.append(i)
+
+    def add_levc(var):
+        result.append(varibale4.get())
+        print(result)
+
+    combobox3 = tk.OptionMenu(window2, varibale4, *val, command=add_levc)
+    combobox3.grid(row=11, column=2)
+
+    lbl_one1 = Label(window2, text='ФИО', bg='#BF7F30').grid(row=5, column=1)
+    lbl_one2 = Label(window2, text='Должность', bg='#BF7F30').grid(row=7, column=1)
+    lbl_one3 = Label(window2, text='Актуальность', bg='#BF7F30').grid(row=8, column=1)
+    lbl_one4 = Label(window2, text='Профессия', bg='#BF7F30').grid(row=9, column=1)
+    lbl_one4 = Label(window2, text='Образование', bg='#BF7F30').grid(row=10, column=1)
+    lbl_one5 = Label(window2, text='Уровень владения ПК', bg='#BF7F30').grid(row=11, column=1)
+
+    # not work
+    def get_vakansia(result):
+        with connection.cursor() as cursor:
+            fiov = txt_1.get()
+            actual = txt_3.get()
+            res = result
+            now = datetime.datetime.now()
+            k = (now.strftime("%Y-%m-%d"))
+            sql = f"INSERT INTO vakansia (data_opubl, kod_dolj, actual, kod_profs, kod_obraz, kod_levc, FIO) " \
+                  f"VALUES('{k}', '{arg1[res[0]]}', '{actual}', '{arg2[res[1]]}', '{arg3[res[2]]}', '{arg4[res[3]]}', '{fiov}')"
+            print(sql)
+            cursor.execute(sql)
+
+    btn_reg = Button(window2, text='Отправить', command=lambda: get_vakansia(result), bg='#BF9930').grid(row=12,
+                                                                                                         column=2)
+    window2.mainloop()
+
 
 root1 = Tk()
 root1.title("Форма для вакансии")
 root1.geometry('300x200+700+300')
 root1.resizable(False, False)
 root1.configure(background='#BF7F30')
-# Лейблы
-# lbl_one0 = Label(root1, text='Логин',bg='#BF7F30').grid(row=6, column=5)
-# lbl_one1 = Label(root1, text='Пароль',bg='#BF7F30').grid(row=8, column=5)
-# # Поля для ввода
-# txt_one = Entry(root1, width=20, bg='#BF9930')
-# txt_one.grid(row=7, column=5)
-# txt_two = Entry(root1, width=20, bg='#BF9930')
-# txt_two.grid(row=9, column=5)
-# Поля для ввода
-# Кнопки
 btn_reg = Button(text='Заполнить заявку', command=zapolnenie, bg='#BF9930')
 btn_reg.place(x=90, y=75)
-# Кнопки
-# таблица
 root1.mainloop()
